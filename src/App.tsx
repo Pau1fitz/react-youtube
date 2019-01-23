46170e2e7f5c0ac071b673d315053ba9e8eeb50b
Paul Fitzgerald <paulfitz99@gmail.com>
2019-01-23 14:55:08 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { normalize } from "polished";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const API_KEY = "AIzaSyAXo-YsLg3RfGrQ3NWIX4D2DYyMF-aleO4";
+const App = () => {
+  const [query, setQuery] = useState<string | undefined>(undefined);
+  const [search, setSearch] = useState("redux");
+  useEffect(
+    () => {
+      fetch(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${API_KEY}`
+      ).then(res => {
+        console.log(res);
+      });
+    },
+    [query]
+  );
+  return (
+    <div>
+      <h4>You Tube</h4>
+      <input
+        type="text"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+
+      <button onClick={() => setQuery(search)}>Search</button>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,66 @@
+import { createGlobalStyle } from "styled-components/macro";
+import { normalize } from "polished";
+
+export const GlobalStyle = createGlobalStyle`
+	${normalize()};
+
+	html, body, div, span,
+	h1, h2, p, a, ul, li,
+	header, main, section,
+	button {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
+	}
+
+	*,
+	*:before,
+	*:after {
+		box-sizing: border-box;
+	}
+
+	html {
+		height: 100%;
+		touch-action: manipulation;
+		-webkit-tap-highlight-color: rgba(0,0,0,0);
+		-webkit-tap-highlight-color: transparent;
+		-ms-overflow-style: -ms-autohiding-scrollbar;
+	}
+
+	body {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		font-size: 16px;
+		line-height: 1.345;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	#root {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+
+	a {
+		color: inherit;
+	}
+
+	button {
+		font-family: inherit;
+		line-height: normal;
+		border-radius: 0;
+		-webkit-font-smoothing: inherit;
+		-moz-osx-font-smoothing: inherit;
+	}
+
+	input {
+		font-family: inherit;
+		-webkit-font-smoothing: inherit;
+		-moz-osx-font-smoothing: inherit;
+	}
+`;
